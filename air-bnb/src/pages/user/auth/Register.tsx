a9cec25a300d8be946010930caf4b53c8f45c25d
huu nhat <nhat200901@gmail.com>
2024-06-10 11:55:44 +0700

    update

--- air-bnb/src/pages/user/auth/Register.tsx
+++ air-bnb/src/pages/user/auth/Register.tsx
@@ -98,17 +98,15 @@ export default function Register() {
                   <input {...register('email')} type="text" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" />
                   <span className='text-danger inline-block pl-1 text-sm'>{formState.errors.email?.message as any}</span>
                 </div>
-                <div className='my-0 grid grid-cols-2 gap-1'>
-                  <div className='my-0 col-span-1 w-100'>
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                    <input {...register('password')} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                    <span className='text-danger inline-block pl-1 text-sm'>{formState.errors.password?.message as any}</span>
-                  </div>
-                  <div className='my-0 col-span-1 w-100'>
-                    <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                    <input {...register('confirmPassword')} type="password" name="confirmPassword" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                    <span className='text-danger inline-block pl-1 text-sm'>{formState.errors.confirmPassword?.message as any}</span>
-                  </div>
+                <div className='my-0'>
+                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                  <input {...register('password')} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                  <span className='text-danger inline-block pl-1 text-sm'>{formState.errors.password?.message as any}</span>
+                </div>
+                <div className='my-0'>
+                  <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                  <input {...register('confirmPassword')} type="password" name="confirmPassword" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                  <span className='text-danger inline-block pl-1 text-sm'>{formState.errors.confirmPassword?.message as any}</span>
                 </div>
                 <div className='my-0'>
                   <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
@@ -120,14 +118,14 @@ export default function Register() {
                   <input {...register('phone')} type="text" name="phone" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" />
                   <span className='text-danger inline-block pl-1 text-sm'>{formState.errors.phone?.message as any}</span>
                 </div>
-                <div className="my-0 w-100">
+                <div className="my-0">
                   <label htmlFor="birthday" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Birthday</label>
-                  <input {...register('birthday')} type="date" name="birthday" id="birthday" className="w-100 bg-gray-50 border border-gray-300 text-gray-900 test-xs sm:text-xs rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" />
+                  <input {...register('birthday')} type="date" name="birthday" id="birthday" className="bg-gray-50 border border-gray-300 text-gray-900 test-xs sm:text-xs rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" />
                   <span className='text-danger inline-block pl-1 text-sm'>{formState.errors.birthday?.message as any}</span>
                 </div>
-                <div className="my-0 w-100">
+                <div className="my-0">
                   <label htmlFor="gender" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
-                  <select {...register('gender')} defaultValue='' name="gender" id="gender" className="w-100 bg-gray-50 border border-gray-300 text-gray-900 test-xs sm:text-xs rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  >
+                  <select {...register('gender')} defaultValue='' name="gender" id="gender" className="bg-gray-50 border border-gray-300 text-gray-900 test-xs sm:text-xs rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  >
                     <option value='' disabled hidden></option>
                     <option value='true'>Nam</option>
                     <option value='false'>Nữ</option>

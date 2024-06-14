fd53db89f0da7e8cd9ad8169af9965fe4a38e73b
huu nhat <nhat200901@gmail.com>
2024-06-14 19:08:55 +0700

    update

--- air-bnb/src/pages/user/home/trang-chu/Carousel.tsx
+++ air-bnb/src/pages/user/home/trang-chu/Carousel.tsx
@@ -12,10 +12,10 @@ export default function Carousel() {
       {/* The slideshow */}
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img className="d-block w-100" src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEyNjIzMTk3NDU3MjE4Nzg2NA%3D%3D/original/f4cbe542-3ce0-4c6f-a8f1-d2120c1b2420.jpeg?im_w=1440&im_q=highq" alt="Los Angeles" />
+          <img className="d-block w-100" src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEyNTQ0NTEyMzEwMTI3NDg1MQ%3D%3D/original/bd73f0f8-9057-4bbc-ad70-1db13eb5c03f.png?im_w=1440&im_q=highq" alt="New York" />
         </div>
         <div className="carousel-item">
-          <img className="d-block w-100" src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEzMTA4OTQ5ODA0MDcwMTE4Mw%3D%3D/original/4f7a276e-9995-4b32-bda6-300de0619b25.png?im_w=720&im_q=highq" alt="Chicago" />
+          <img className="d-block w-100" src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEyNTQ0NTEyMzEwMTI3NDg1MQ%3D%3D/original/bd73f0f8-9057-4bbc-ad70-1db13eb5c03f.png?im_w=1440&im_q=highq" alt="New York" />
         </div>
         <div className="carousel-item">
           <img className="d-block w-100" src="https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTEyNTQ0NTEyMzEwMTI3NDg1MQ%3D%3D/original/bd73f0f8-9057-4bbc-ad70-1db13eb5c03f.png?im_w=1440&im_q=highq" alt="New York" />

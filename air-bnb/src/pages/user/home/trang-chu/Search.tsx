0a1a8fffe507d1dabafab1f7d9db22d10f771262
huu nhat <nhat200901@gmail.com>
2024-06-15 10:34:15 +0700

    udpate

--- air-bnb/src/pages/user/home/trang-chu/Search.tsx
+++ air-bnb/src/pages/user/home/trang-chu/Search.tsx
@@ -2,26 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './css/Search.css';
 
 export default function Search() {
-  // const [hidden, setHidden] = useState(true);
-  // const [prevScrollY, setPrevScrollY] = useState(0);
+  const [hidden, setHidden] = useState(false);
+  const [prevScrollY, setPrevScrollY] = useState(0);
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const currentScrollY = window.scrollY;
-  //     setHidden(currentScrollY > prevScrollY);
-  //     setPrevScrollY(currentScrollY);
-  //   };
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      setHidden(currentScrollY > prevScrollY);
+      setPrevScrollY(currentScrollY);
+    };
 
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, [prevScrollY]);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollY]);
 
   return (
     <>
       <form id='search' >
-        <input type="text" placeholder="Search.." name="search" />
+        <input type="text" className={`${hidden ? 'tat' : 'bat'}`} placeholder="Tìm kiếm..." name="search" />
         {/* <button type="submit"><i className="fa fa-search" /></button> */}
       </form>
     </>

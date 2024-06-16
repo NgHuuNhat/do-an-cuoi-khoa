5ccf4a8b588e8565af45336e7bda52bf694e67f6
huu nhat <nhat200901@gmail.com>
2024-06-16 22:05:19 +0700

    update

--- air-bnb/src/pages/user/home/trang-chu/kham-pha/KhamPha.tsx
+++ air-bnb/src/pages/user/home/trang-chu/kham-pha/KhamPha.tsx
@@ -3,8 +3,9 @@ import './css/KhamPha.css'
 
 export default function KhamPha() {
     return (
+        // <></>
         <div id='khamPha' className='container'>
-            <div className="mt-5 bg-primary flex flex-wrap justify-center gap-3">
+            <div className="my-5 p-1 bg-light flex flex-wrap justify-center gap-3">
                 <button className="rounded-lg text-md bg-white text-black border border-gray-300 hover:border-gray-900 duration-300 px-6 py-2">
                     Loại nơi ở
                 </button>
@@ -22,13 +23,13 @@ export default function KhamPha() {
                 </button>
             </div>
 
-            <div className="py-5 bg-success grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div className="my-5 p-1 bg-light grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                 <a href="/rooms/ho-chi-minh" className="w-full ant-card ant-card-bordered ant-card-hoverable flex items-center cursor-pointer hover:bg-gray-100 hover:scale-105 transition duration-300 ease-in-out css-mzwlov">
                     <div className="ant-card-body">
                         <div className="flex items-center gap-3">
                             <img src="https://res.cloudinary.com/rawn/image/upload/hnevi0eqxhxjgh6skplj.webp" alt='' className="w-12 h-12 rounded-lg object-cover" />
                             <div>
-                                <h2 className="font-bold">Hồ Chí Minh</h2>
+                                <h2 className="font-bold" style={{ fontSize: '1rem', color: 'black' }}>Hồ Chí Minh</h2>
                                 <p className="text-gray-700 text-sm">15 phút lái xe</p>
                             </div>
                         </div>
@@ -39,7 +40,7 @@ export default function KhamPha() {
                         <div className="flex items-center gap-3">
                             <img src="https://res.cloudinary.com/rawn/image/upload/lbe3gpqkrwmzt98ce2nj.webp" alt='' className="w-12 h-12 rounded-lg object-cover" />
                             <div>
-                                <h2 className="font-bold">Cần Thơ</h2>
+                                <h2 className="font-bold" style={{ fontSize: '1rem', color: 'black' }}>Cần Thơ</h2>
                                 <p className="text-gray-700 text-sm">3 giờ lái xe</p>
                             </div>
                         </div>
@@ -50,7 +51,7 @@ export default function KhamPha() {
                         <div className="flex items-center gap-3">
                             <img src="https://res.cloudinary.com/rawn/image/upload/xi99sldgebhfvd3n66yx.webp" alt='' className="w-12 h-12 rounded-lg object-cover" />
                             <div>
-                                <h2 className="font-bold">Nha Trang</h2>
+                                <h2 className="font-bold" style={{ fontSize: '1rem', color: 'black' }}>Nha Trang</h2>
                                 <p className="text-gray-700 text-sm">6.5 giờ lái xe</p>
                             </div>
                         </div>
@@ -61,7 +62,7 @@ export default function KhamPha() {
                         <div className="flex items-center gap-3">
                             <img src="https://res.cloudinary.com/rawn/image/upload/hnevi0eqxhxjgh6skplj.webp" alt='' className="w-12 h-12 rounded-lg object-cover" />
                             <div>
-                                <h2 className="font-bold">Hà Nội</h2>
+                                <h2 className="font-bold" style={{ fontSize: '1rem', color: 'black' }}>Hà Nội</h2>
                                 <p className="text-gray-700 text-sm">15 phút lái xe</p>
                             </div>
                         </div>
@@ -72,7 +73,7 @@ export default function KhamPha() {
                         <div className="flex items-center gap-3">
                             <img src="https://res.cloudinary.com/rawn/image/upload/v1skk44cynr7gauhzb4e.webp" alt='' className="w-12 h-12 rounded-lg object-cover" />
                             <div>
-                                <h2 className="font-bold">Phú Quốc</h2>
+                                <h2 className="font-bold" style={{ fontSize: '1rem', color: 'black' }}>Phú Quốc</h2>
                                 <p className="text-gray-700 text-sm">7.5 giờ lái xe</p>
                             </div>
                         </div>
@@ -83,7 +84,7 @@ export default function KhamPha() {
                         <div className="flex items-center gap-3">
                             <img src="https://res.cloudinary.com/rawn/image/upload/tqrm3cthowneesuafbp0.webp" alt='' className="w-12 h-12 rounded-lg object-cover" />
                             <div>
-                                <h2 className="font-bold">Đà Nẵng</h2>
+                                <h2 className="font-bold" style={{ fontSize: '1rem', color: 'black' }}>Đà Nẵng</h2>
                                 <p className="text-gray-700 text-sm">45 phút lái xe</p>
                             </div>
                         </div>
@@ -94,7 +95,7 @@ export default function KhamPha() {
                         <div className="flex items-center gap-3">
                             <img src="https://res.cloudinary.com/rawn/image/upload/tgt8dxlfwdh41jkptxeg.webp" alt='' className="w-12 h-12 rounded-lg object-cover" />
                             <div>
-                                <h2 className="font-bold">Đà Lạt</h2>
+                                <h2 className="font-bold" style={{ fontSize: '1rem', color: 'black' }}>Đà Lạt</h2>
                                 <p className="text-gray-700 text-sm">30 phút lái xe</p>
                             </div>
                         </div>
@@ -105,7 +106,7 @@ export default function KhamPha() {
                         <div className="flex items-center gap-3">
                             <img src="https://res.cloudinary.com/rawn/image/upload/bt5jrxsl5ljq5bmfqqw0.webp" alt='' className="w-12 h-12 rounded-lg object-cover" />
                             <div>
-                                <h2 className="font-bold">Phan Thiết</h2>
+                                <h2 className="font-bold" style={{ fontSize: '1rem', color: 'black' }}>Phan Thiết</h2>
                                 <p className="text-gray-700 text-sm">5 giờ lái xe</p>
                             </div>
                         </div>

de7045c464455f53ed6cf5a7c4a091cac9040ff2
huu nhat <nhat200901@gmail.com>
2024-06-19 16:14:22 +0700

    update

--- air-bnb/src/pages/user/home/trang-chu/search-demo/SearchDemo.tsx
+++ air-bnb/src/pages/user/home/trang-chu/search-demo/SearchDemo.tsx
@@ -56,7 +56,7 @@ export default function SearchDemo() {
             <form action="" onSubmit={handleSubmit(onSubmit)}>
                 <div className="search-demo custom-shadow grid grid-cols-10 smm:grid-cols-1 border-1 border-gray-300 md:rounded-full">
                     <div className="col-span-3 flex-1 p-3 flex flex-col justify-center items-center cursor-pointer">
-                        <label htmlFor='diaDiem' className=''>
+                        <label htmlFor='diaDiem' className='mb-0'>
                             <p style={{ fontWeight: '500' }} className="text-sm">Địa điểm</p>
                             <div className="search-none smm:border-b md:hidden smm:border-gray-400 smm:w-9/12 py-2" />
                         </label>
@@ -76,12 +76,12 @@ export default function SearchDemo() {
                         <div className="search-none smm:border-b md:hidden smm:border-gray-400 smm:w-9/12 py-2" />
                         <div className='grid grid-cols-2 gap-1 w-100'>
                             <div className='w-100'>
-                                <p style={{ fontWeight: '500', textAlign: 'center' }} className='text-sm'>Ngày đi</p>
-                                <input {...register('ngayDi')} type="date" className="form-control text-center" id="ngayDi" />
+                                <p style={{ fontWeight: '500', textAlign: 'center', marginBottom: '8px !important' }} className='text-sm'>Ngày đi</p>
+                                <input {...register('ngayDi')} type="date" className="form-control text-center" id="ngayDi" style={{ outline: 'none' }} />
                             </div>
                             <div>
                                 <p style={{ fontWeight: '500', textAlign: 'center' }} className='text-sm'>Ngày về</p>
-                                <input {...register('ngayVe')} type="date" className="form-control text-center" id="ngayVe" />
+                                <input {...register('ngayVe')} type="date" className="form-control text-center" id="ngayVe" style={{ outline: 'none' }} />
                             </div>
                         </div>
                         {/* <span className='text-danger inline-block pl-1 text-sm'></span> */}
@@ -92,7 +92,7 @@ export default function SearchDemo() {
 
                     <div className="search-block col-span-3 flex-1 p-3 flex justify-center items-center cursor-pointer relative gap-3">
                         <div className='grid grid-cols-1'>
-                            <label htmlFor='soLuong' className=''>
+                            <label htmlFor='soLuong' className='mb-0'>
                                 <p style={{ fontWeight: '500' }} className="text-sm">Thêm khách</p>
                             </label>
                             <select {...register('soLuong')} id="soLuong" className="form-control" defaultValue='' style={{ fontSize: 'small' }}>
@@ -101,7 +101,7 @@ export default function SearchDemo() {
                                 <option>2</option>
                                 <option>3</option>
                                 <option>4</option>
-                                <option>4</option>
+                                <option>5</option>
                             </select>
                             {/* <span className='text-danger inline-block pl-1 text-sm'></span> */}
                         </div>
